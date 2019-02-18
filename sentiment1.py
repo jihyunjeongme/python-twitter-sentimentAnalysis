471e87103c7900be59624f4aa832e4c5b5e38820
jihyunjeongme <jihyunjeong.me@gmail.com>
2019-02-18 22:23:59 +0900

    Initial commit

---
+++ sentiment1.py
@@ -0,0 +1,9 @@
+#Import libraries
+import tweepy
+from textblob import TextBlob
+
+#Create your app from apps.twitter.com and fill your keys and tokens
+consumer_key = '3KFL*************'
+consumer_secret = 'yltO********************'
+access_token = '3014895**************'
+access_token_secret = 'w7rZ********************'
